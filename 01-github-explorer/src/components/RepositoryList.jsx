c77cb7d0c7d0135b48933631c8b118bc295048ed
Gustavo Castro <gustavocastrocs@gmail.com>
2021-09-21 08:57:28 -0300

    Chamadas HTTP

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,21 +1,32 @@
 import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform' 
-}
 export function RepositoryList(){
+    const [repositories, setRepositories] = useState([]) //armazenando uma listagens de repos
+    //repositories: variavel
+    //setRepositories: funcao para alterar o valor da variavel repositories
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, []);
+    //useEffect()
+        //1parametro: qual função eu desejo executar 
+        //2parametro: quando eu quero executar a primeira funcao
+        //Se eu passar um array vazio como segundo parametro a primeira funcao sera executada apenas uma unica vez
+
+    console.log(repositories)
     return(
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
             </ul>
         </section>
-    )
+    );
 }
