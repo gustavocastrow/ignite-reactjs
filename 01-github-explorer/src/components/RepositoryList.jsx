4288be3e59a1de74aaf3074feb0fb2e4ecfc4a78
Gustavo Castro <gustavocastrocs@gmail.com>
2021-10-01 09:42:39 -0300

    Chamadas HTTP

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,20 +1,26 @@
+import { useEffect, useState } from 'react'; 
 import { RepositoryItem } from "./RepositoryItem"
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-}
+import '../styles/repositories.scss';
 
 export function RepositoryList(){
+
+    const [repositories, setRepositories] = useState([]); //sempre iniciar o useState com o mesmo tipo de dados da varivel
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, []);
+
+    console.log(repositories);
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-               <RepositoryItem repository={repository}/> 
-               <RepositoryItem repository={repository}/>
-               <RepositoryItem repository={repository}/>
-
+                {repositories.map(repository => {
+                   return <RepositoryItem key={repository.name} repository={repository} />
+                })}
             </ul>
         </section>
     )
