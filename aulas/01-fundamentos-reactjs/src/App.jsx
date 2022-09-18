4f8c0778e94f62c385804db232752a003d317b51
gustavo castro <gustavocastrocs@gmail.com>
2022-09-18 20:48:27 -0300

    Finalizando Sidebar

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,22 +1,31 @@
-import { Header } from './components/Header'
-import { Post } from './Post'
+import { Header } from './components/Header';
+import { Post } from './Post';
+import styles from  './App.module.css';
 import './global.css'
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Gustavo Castro"
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, reprehenderit. Maxime odit nisi quam amet aliquid, nostrum itaque voluptas laborum, autem reprehenderit asperiores et, eos quis voluptate temporibus rem natus!"
-        location="São José dos Campos - SP"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar/>
 
-      <Post 
-        author="Diego Fernandes"
-        content="React é top"
-        location="Rio do Sul - SC"
-      />
+        <main>
+          <Post 
+            author="Gustavo Castro"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, reprehenderit. Maxime odit nisi quam amet aliquid, nostrum itaque voluptas laborum, autem reprehenderit asperiores et, eos quis voluptate temporibus rem natus!"
+            location="São José dos Campos - SP"
+          />
+          <Post 
+            author="Diego Fernandes"
+            content="React é top"
+            location="Rio do Sul - SC"
+            />
+        </main>
+
+      </div>
+     
     </div>
     
   )
