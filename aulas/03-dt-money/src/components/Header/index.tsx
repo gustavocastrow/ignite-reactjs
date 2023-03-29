43fe9e4bb5c1e47e290cb4c050f013e8d8ba595d
Gustavo Castro <gustavocastrocs@gmail.com>
2023-03-29 20:23:12 +0200

    feat: modal

--- aulas/03-dt-money/src/components/Header/index.tsx
+++ aulas/03-dt-money/src/components/Header/index.tsx
@@ -1,12 +1,21 @@
 import { HeaderCointainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from '../../assets/logo.svg';
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header(){
   return (
     <HeaderCointainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderCointainer>
   )
