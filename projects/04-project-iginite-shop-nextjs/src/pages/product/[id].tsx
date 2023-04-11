8f885ccd31ed05b8a9285a38c2a1f06dac4eda6d
Gustavo Castro <gustavocastrocs@gmail.com>
2023-04-11 20:31:24 +0200

    feat: SSG with dynamic parameter

--- projects/04-project-iginite-shop-nextjs/src/pages/product/[id].tsx
+++ projects/04-project-iginite-shop-nextjs/src/pages/product/[id].tsx
@@ -1,22 +1,69 @@
 import { ImageContainter, ProductContainer, ProductDetails } from '@/styles/pages/product'
-import {useRouter} from 'next/router'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { stripe } from '../../lib/stripe'
+import Stripe from 'stripe'
+import Image from 'next/image'
 
-export default function Product(){
-  const { query } = useRouter()
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+  }
+}
+
+export default function Product({product}: ProductProps){
   return(
     <ProductContainer>
       <ImageContainter>
-
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
       </ImageContainter>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente voluptatibus nesciunt, animi neque saepe commodi, culpa perferendis vitae odio dicta laudantium ratione explicabo distinctio. Cum id necessitatibus perferendis quibusdam illo!</p>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
 
         <button>Comprar Agora</button>
       
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return{
+    paths: [
+      {params: {id: 'prod_NfEc2zy9Sn5pX9'}}
+    ],
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps<any, {id: string}> = async ({params}) =>{
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  });
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount / 100),
+        description: product.description,
+      }
+    },
+    revalidate: 60 * 60 * 1, //1hour in cache
+  }
 }
